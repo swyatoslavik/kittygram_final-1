0c43762b13a5eaf0d4c8471d31f529eb4ee21e3d
Фархад Гараев <shokvee@gmail.com>
2023-07-26 17:14:18 +0300

    fix deploy

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['kittigram.myftp.org', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['51.250.17.77', '127.0.0.1', 'localhost', 'kittigram.myftp.org']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
